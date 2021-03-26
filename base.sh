7101586abdf07eab73c3f8f7eb5be0aef0c83c30
flashbtw <johannes-kurt.reiter@freenet.de>
2021-03-26 09:02:32 +0100

    improved pwd protection

--- base.sh
+++ base.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-PASSWORD=`cat /home/flashzboi/autobackup/pwd.password`
+PASSWORD=`cat /home/flashzboi/autobackup/pwd.pasword`
+#checking if password file exists
+
+
+#checking if password file is empty
+if [ "$PASSWORD" != "" ]
+then
+  echo "Password detected."
+else
+  echo "No Password detected. Exiting..."
+  exit
+fi
+
+if [ "$PASSWORD" != " " ]
+then
+  echo "Password detected."
+else
+  echo "No Password detected. Exiting..."
+fi
 
 read -s -p "Password: " MYPASSWORD
 if [ "$MYPASSWORD" == "$PASSWORD"  ]
