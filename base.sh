3efa133f2b027973c7700b3544b574b12a0cb1e4
flashbtw <johannes-kurt.reiter@freenet.de>
2021-03-27 22:39:12 +0100

    mc dir creation and logging

--- base.sh
+++ base.sh
@@ -8,15 +8,33 @@ if [ "$WHOAMI" != "root" ]; then
 fi
 
 PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-/usr/bin/rm $PATH/log/errors.txt
+ERRORLOG="$PATH/log/errors.txt"
+LOG="$PATH/log/log.txt"
+
+#some custom commands
+logecho()
+(
+    echo "$@"
+    echo "$@" >>$LOG
+)
+
+logprintf()
+(
+    printf "$@"
+    printf "$@"
+)
+
+#removing old logs
+/usr/bin/rm $ERRORLOG 2>>/dev/null
+/usr/bin/rm $LOG 2>>/dev/null
 
 #catching error messages
 trap 'catch $? $LINENO' ERR
+trap 'clean' ERR
 
 catch() {
   echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
 }
-
 
 PASSWORD=`/usr/bin/cat /home/flashzboi/autobackup/pwd.password 2>>$PATH/log/errors.txt`
 
@@ -32,15 +50,39 @@ fi
 read -s -p "Password: " MYPASSWORD
 if [ "$MYPASSWORD" == "$PASSWORD"  ]
 then
-  printf "\nHallo :-)\n"
+  logprintf "\nServer restore will begin in a few sceonds. If you want to abort this action, press STRG + C\n"
 else
-  printf "\nNö.\n"
+  logprintf "\nPassword denied.\n"
   exit
 fi
-{
-/usr/bin/cat /gibs/nich/
-} 2>>$PATH/log/errors.txt
 
-{
-/usr/bin/cat /does/not/exist/
-} 2>>$PATH/log/errors.txt
+#checking if directorys already exist
+
+SERVERDIR="/home/mc/"
+
+clean() {
+>/dev/null
+}
+#creating the main home directory
+SERVERDIRTEST=`/usr/bin/ls $SERVERDIR 2>>/dev/null`
+
+catch() {
+  echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
+}
+
+if [ "$SERVERDIRTEST" == "" ]; then
+  {
+  /usr/bin/mkdir /home/mc/
+  /usr/bin/touch /home/mc/initial.indicator
+  } 2>>$ERRORLOG
+  SERVERDIRTEST2=`/usr/bin/ls $SERVERDIR 2>>/dev/null`
+ 
+  if [ "$SERVERDIRTEST2" == "" ]; then
+    logecho "Home Directory can not be created"
+  else
+    logecho "Home Directory successfully created"
+  fi
+
+else
+  logecho "Home Directory already exists!"
+fi
