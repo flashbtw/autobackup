c9e5fb05f73540f0c41b4af29a3e3d56e1454e37
flashbtw <johannes-kurt.reiter@freenet.de>
2021-03-29 19:53:06 +0200

    Main Directory, Error Logging Improvement

--- base.sh
+++ base.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#User Verification
 WHOAMI=`whoami`
 
 if [ "$WHOAMI" != "root" ]; then
@@ -7,11 +8,14 @@ if [ "$WHOAMI" != "root" ]; then
   exit
 fi
 
+#Path of Script
 PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+#Path of Error-Log-File
 ERRORLOG="$PATH/log/errors.txt"
+#Path of Log-File
 LOG="$PATH/log/log.txt"
 
-#some custom commands
+#custom commands for logging
 logecho()
 (
     echo "$@"
@@ -30,12 +34,11 @@ logprintf()
 
 #catching error messages
 trap 'catch $? $LINENO' ERR
-trap 'clean' ERR
-
 catch() {
   echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
 }
 
+#Password Reading
 PASSWORD=`/usr/bin/cat /home/flashzboi/autobackup/pwd.password 2>>$PATH/log/errors.txt`
 
 #checking if password file is empty
@@ -47,24 +50,30 @@ else
   exit
 fi
 
+#Prompting for Password and Checking
 read -s -p "Password: " MYPASSWORD
 if [ "$MYPASSWORD" == "$PASSWORD"  ]
 then
-  logprintf "\nServer restore will begin in a few sceonds. If you want to abort this action, press STRG + C\n"
+  logprintf "\nServer restore will begin in a few sceonds. If you want to abort this action, press CTRL + C\n"
+  {
+  /usr/bin/sleep 1
+  logprintf "3...\n"
+  /usr/bin/sleep 1
+  logprintf "2...\n"
+  /usr/bin/sleep 1
+  logprintf "1...\n"
+  /usr/bin/sleep 1
+  logprintf "No Abortion detected. Restoring the server...\n"
+  } 2>>$ERRORLOG
 else
-  logprintf "\nPassword denied.\n"
+  logprintf "\nAuthentication Failed."
   exit
 fi
 
 #checking if directorys already exist
-
-SERVERDIR="/home/mc/"
-
-clean() {
->/dev/null
-}
 #creating the main home directory
-SERVERDIRTEST=`/usr/bin/ls $SERVERDIR 2>>/dev/null`
+SERVERDIR="/home/mc"
+SERVERDIRTEST=`/usr/bin/ls $SERVERDIR`
 
 catch() {
   echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
@@ -76,7 +85,7 @@ if [ "$SERVERDIRTEST" == "" ]; then
   /usr/bin/touch /home/mc/initial.indicator
   } 2>>$ERRORLOG
   SERVERDIRTEST2=`/usr/bin/ls $SERVERDIR 2>>/dev/null`
- 
+
   if [ "$SERVERDIRTEST2" == "" ]; then
     logecho "Home Directory can not be created"
   else
