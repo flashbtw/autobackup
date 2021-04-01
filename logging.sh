6263514d4e546358260a27ee8fe2e0dbdda5eb45
flashbtw <johannes-kurt.reiter@freenet.de>
2021-04-01 21:09:09 +0200

    script improvement

--- logging.sh
+++ logging.sh
@@ -6,38 +6,17 @@ DATE=`printf '%(%Y-%m-%d)T\n' -1`
 PATH_LOG_DIRECTORY="/home/flashzboi/autobackup/logtest"
 DATE_IN_FILE=`/usr/bin/cat $PATH_LOG_DIRECTORY/date.date`
 PATH_LOGNUMBER="$PATH_LOG_DIRECTORY/next.number"
-PATH_LOGNUMBER_IND="$PATH_LOG_DIRECTORY/number.indicator"
 PATH_DATE="$PATH_LOG_DIRECTORY/date.date"
-PATH_DATE_IND="$PATH_LOG_DIRECTORY/date.indicator"
 /usr/bin/echo $PATH_LOG_DIRECTORY
 /usr/bin/echo $PATH_LOGNUMBER
 /usr/bin/echo $PATH_LOGNUMBER_IND
 /usr/bin/echo "Today is the $DATE"
 
-#Log Number Detection
-#if `/usr/bin/cp $PATHLOGNUMBER $PATHLOGNUMBERIND` ; then
-#  /usr/bin/echo "Command succeeded"
-#  /usr/bin/sleep 1
-#  NUMBER=`/usr/bin/cat $PATHLOGNUMBER`
-#  NEXTNUMBER=`/usr/bin/expr $NUMBER + 1`
-#  {
-#  /usr/bin/echo $NEXTNUMBER
-#  } >$PATHLOGNUMBER
-#  /usr/bin/rm $PATHLOGNUMBERIND
-#else
-#  /usr/bin/echo "Command failed"
-#  /usr/bin/sleep 3
-#  /usr/bin/touch $PATHLOGNUMBER
-#  {
-#  /usr/bin/echo "1"
-#  } 2>$PATHLOGNUMBER
-#fi
 
-if `/usr/bin/cp $PATH_DATE $PATH_DATE_IND` ; then
+if `/usr/bin/test -f $PATH_DATE` ; then
   /usr/bin/echo "Date File exists"
-  /usr/bin/rm $PATH_DATE_IND
-  if [ `/usr/bin/cat $PATH_DATE` == $DATE ]; then
-    if `/usr/bin/cp $PATH_LOGNUMBER $PATH_LOGNUMBER_IND` ; then
+  if [ `/usr/bin/cat $PATH_DATE` == "$DATE" ]; then
+    if `/usr/bin/test -f $PATH_LOGNUMBER` ; then
       /usr/bin/echo "Log Number File exists"
       /usr/bin/sleep 1
       NUMBER=`/usr/bin/cat $PATH_LOGNUMBER`
@@ -45,7 +24,6 @@ if `/usr/bin/cp $PATH_DATE $PATH_DATE_IND` ; then
       {
       /usr/bin/echo $NEXT_NUMBER
       } >$PATH_LOGNUMBER
-      /usr/bin/rm $PATH_LOGNUMBER_IND
     else
       echo "Creating Log Number File"
       /usr/bin/sleep 1
@@ -70,6 +48,23 @@ else
   {
   /usr/bin/echo "$DATE"
   } >$PATH_DATE
+  if `/usr/bin/test -f $PATH_LOGNUMBER` ; then
+     /usr/bin/echo "Log Number File exists"
+     /usr/bin/sleep 1
+     NUMBER=`/usr/bin/cat $PATH_LOGNUMBER`
+     NEXT_NUMBER=`/usr/bin/expr $NUMBER + 1`
+     {
+     /usr/bin/echo $NEXT_NUMBER
+     } >$PATH_LOGNUMBER
+   else
+     echo "Creating Log Number File"
+     /usr/bin/sleep 1
+     /usr/bin/touch $PATH_LOGNUMBER
+     {
+     /usr/bin/echo "1"
+     } >$PATH_LOGNUMBER
+     LOGNUMBER=`/usr/bin/cat $PATH_LOGNUMBER`
+   fi
 fi
 #Date Log Packaging
 #{
