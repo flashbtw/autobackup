8224bc57c66098bb61b7208e5d8ca9dc930d13af
flashbtw <johannes-kurt.reiter@freenet.de>
2021-04-02 15:29:57 +0200

    saving, script doesn't work properly at the moment

--- base.sh
+++ base.sh
@@ -1,44 +1,22 @@
 #!/bin/bash
+export CONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/config.sh"
+export LOGECHO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/logecho.sh"
+export LOGPRINTF="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/logprintf.sh"
+export DATE_IN_FILE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/date_in_file.sh"
+source $CONFIG
+source $LOGECHO
+source $LOGPRINTF
+source $DATE_IN_FILE
 
-#User Verification
 WHOAMI=`whoami`
-
+#User Verification
 if [ "$WHOAMI" != "root" ]; then
   echo "Script must be run as root or sudo-er"
   exit
 fi
 
-#Password Reading
-PASSWORD=`/usr/bin/cat /home/flashzboi/autobackup/pwd.password 2>>$ERROR_LOG`
-#Path of Script
-PFAD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-#Path of Error-Log-File
-ERROR_LOG="$PFAD/log/errors.txt"
-#Path of Log-File
-LOG="$PFAD/log/log.txt"
-
-#custom commands for logging
-logecho()
-(
-    echo "$@"
-    echo "$@" >>$LOG
-)
-
-logprintf()
-(
-    printf "$@"
-    printf "$@" >>$LOG
-)
-
-#removing old logs
-/usr/bin/rm $ERROR_LOG 2>>/dev/null
-/usr/bin/rm $LOG 2>>/dev/null
-
 #catching error messages
-trap 'catch $? $LINENO' ERR
-catch() {
-  echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
-}
+errorcatcher
 
 #checking if password file is empty
 if [ "$PASSWORD" != "" ]
@@ -53,8 +31,9 @@ fi
 read -s -p "Password: " TYPED_PASSWORD
 if [ "$TYPED_PASSWORD" == "$PASSWORD"  ]
 then
-  logprintf "\nServer restore will begin in a few sceonds. If you want to abort this action, press CTRL + C\n"
   {
+  logprintf "\n "
+  logprintf "\nServer restore will begin in a few sceonds. If you want to abort this action, press CTRL + C\n"
   /usr/bin/sleep 1
   logprintf "3...\n"
   /usr/bin/sleep 1
@@ -63,34 +42,15 @@ then
   logprintf "1...\n"
   /usr/bin/sleep 1
   logprintf "No Abortion detected. Restoring the server...\n"
-  } 2>>$ERROR_LOG
+  }
 else
   logprintf "Authentication Failed.\n"
   exit
 fi
 
-#checking if directorys already exist
-#creating the main home directory
-SERVER_DIR="/home/mc"
-SERVER_DIR_TEST=`/usr/bin/ls $SERVER_DIR`
+#logging
+bash $PFAD/logging.sh
 
-catch() {
-  echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
-}
-
-if [ "$SERVER_DIR_TEST" == "" ]; then
-  {
-  /usr/bin/mkdir /home/mc/
-  /usr/bin/touch /home/mc/initial.indicator
-  } 2>>$ERROR_LOG
-  SERVER_DIR_TEST2=`/usr/bin/ls $SERVER_DIR 2>>/dev/null`
-
-  if [ "$SERVER_DIR_TEST2" == "" ]; then
-    logecho "Home Directory can not be created"
-  else
-    logecho "Home Directory successfully created"
-  fi
-
-else
-  logecho "Home Directory already exists!"
-fi
+#directory creation
+#main.sh
+bash $PFAD/directories/main.sh
---
+++ config.sh
@@ -0,0 +1,44 @@
+PFAD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+PASSWORD=`/usr/bin/cat $PFAD/pwd.password`
+
+### Date ###
+DATE_PATH="$PFAD/date.date"
+### Log ###
+LOG_NUMBER=`/usr/bin/cat $PFAD/logs/next.number`
+LOG_DIRECTORY="$PFAD/logs/log/"
+LOG_NAME="latest$LOG_NUMBER.log"
+LOG_LOCATION="$PFAD/logs/log/$LOG_NAME"
+
+### Error Log ###
+ERROR_LOG_DIRECTORY="$PFAD/logs/errorlog"
+ERROR_LOG_NAME="error$LOG_NUMBER.log"
+ERROR_LOG_LOCATION="$PFAD/logs/errorlog/$ERROR_LOG_NAME"
+
+
+##### FUNCTIONS #####
+
+### Error Catch ###
+function errorcatcher {
+  trap 'catch $? $LINENO' ERR
+  catch() {
+    echo "Error occured in Line $2: For more information look in errors.txt file in log directory"
+  }
+}
+
+### Logging ###
+function logprintf {
+logprintf()
+(
+    printf "$@"
+    printf "$@" >>$LOG_LOCATION
+)
+}
+
+function logecho {
+logecho()
+(
+    echo "$@"
+    echo "$@" >>$LOG_LOCATION
+    echo "Moin"
+)
+}
